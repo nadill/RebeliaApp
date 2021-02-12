1d06a951b1f7f8e8bba8940f9b6e88ce6f748b0c
Michał Mikulski <m.mikulski@vp.pl>
2021-02-12 23:50:50 +0100

    Initial commit

---
+++ RebeliaApp.Web/Dto/Shared/IResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using static RebeliaApp.Web.Model.Enums;
+
+namespace RebeliaApp.Web.Dto.Shared
+{
+    public abstract class ResponseBase
+    {
+        public RESPONSE_CODE ResponseCode { get; set; }
+        public string Message { get; set; }
+    }
+}
